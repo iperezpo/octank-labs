eb9958a7cda79b47d985e043b7064efc8272c97d
EC2 Default User <ec2-user@ip-172-31-30-153.us-west-2.compute.internal>
2016-09-06 21:49:51 +0000

    Modified node.js

--- node-app/app.js
+++ node-app/app.js
@@ -3,9 +3,11 @@ var http = require('http');
 
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
+  userCount++;
   response.writeHead(200, {"Content-Type": "text/html"});
   response.write("<h1>Hello World from Node.js app!!!</h1>\n");
   response.end("<script type=\"text/javascript\">document.write('<h3><a href=\"http://' + location.host + ':8080/\">' + 'Tomcat Application on port 8080</a></h3>');</script>");
+var userCount = 0;
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
