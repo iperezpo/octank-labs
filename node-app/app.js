7332d9e3dc4e021252bf5bee155ae6ec2e2325e2
EC2 Default User <ec2-user@ip-172-31-30-153.us-west-2.compute.internal>
2016-09-06 23:18:22 +0000

    Fixed userCount function

--- node-app/app.js
+++ node-app/app.js
@@ -9,7 +9,7 @@ var server = http.createServer(function (request, response) {
   userCount++;
   response.writeHead(200, {"Content-Type": "text/html"});
   response.write("<h1>Hello World from Node.js app!</h1>\n");
-  response.write("<h3>We have had '+userCount+' visits.</h3>\n");
+  response.write("<h3>We have had "+userCount+" visits.</h3>\n");
   response.end("<script type=\"text/javascript\">document.write('<h3><a href=\"http://' + location.host + ':8080/\">' + 'Tomcat Application on port 8080</a></h3>');</script>");
 });
 
