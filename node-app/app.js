0445e1aeeae91d37ef0442a34a50d2d0b8a21306
EC2 Default User <ec2-user@ip-172-31-30-153.us-west-2.compute.internal>
2016-09-06 22:44:41 +0000

    Added counter function to app.js

--- node-app/app.js
+++ node-app/app.js
@@ -1,13 +1,16 @@
 // Load the http module to create an http server.
 var http = require('http');
 
+// Var definition for userbytwo variable
+var userCount = 0;
+
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
   userCount++;
   response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("<h1>Hello World from Node.js app!!!</h1>\n");
+  response.write("<h1>Hello World from Node.js app!</h1>\n");
+  response.write("<h3>We have had '+userCount+' visits.</h3>\n");
   response.end("<script type=\"text/javascript\">document.write('<h3><a href=\"http://' + location.host + ':8080/\">' + 'Tomcat Application on port 8080</a></h3>');</script>");
-var userCount = 0;
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
